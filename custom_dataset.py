1496643aa9de1ac8d2ebfda6ce4f730393126d79
Mdhvince <vinceslas.medhy@gmail.com>
2019-03-31 20:20:55 +0200

    custom dataset done

---
+++ custom_dataset.py
@@ -0,0 +1,50 @@
+import glob
+import os
+import torch
+from torch.utils.data import Dataset, DataLoader
+import numpy as np
+import matplotlib.image as mpimg
+import pandas as pd
+import cv2
+
+
+
+class FacialKeypointsDataset(Dataset):
+    """Face Landmarks dataset."""
+
+    def __init__(self, csv_file, root_dir, transform=None):
+        """
+        Args:
+            csv_file (string): Path to the csv file with annotations.
+            root_dir (string): Directory with all the images.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        """
+        self.key_pts_frame = pd.read_csv(csv_file)
+        self.root_dir = root_dir
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.key_pts_frame)
+
+    def __getitem__(self, idx):
+        
+        # take the image name contained in the csv file
+        image_name = os.path.join(self.root_dir,
+                                self.key_pts_frame.iloc[idx, 0])
+        
+        # read the true image based on that name
+        image = mpimg.imread(image_name)
+        
+        # if image has an alpha color channel, get rid of it
+        if(image.shape[2] == 4):
+            image = image[:,:,0:3]
+        
+        key_pts = self.key_pts_frame.iloc[idx, 1:].as_matrix()
+        key_pts = key_pts.astype('float').reshape(-1, 2)
+        sample = {'image': image, 'keypoints': key_pts}
+
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample
