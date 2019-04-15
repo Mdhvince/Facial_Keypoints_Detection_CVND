28e1f1b3f02679ce750cff4244945de13232c840
Mdhvince <vinceslas.medhy@gmail.com>
2019-04-15 19:29:54 +0200

    python file helper updated

---
+++ helper.py
@@ -0,0 +1,22 @@
+
+def visualize(num_images, train_loader, row, col):
+  # obtain one batch of training images
+  batch = next(iter(train_loader))
+
+  # display n images
+  for i in np.arange(num_images):
+
+      images, labels = batch['image'], batch['keypoints']
+
+      #unormalize images
+      image = images[i].numpy()
+      image = np.transpose(image, (1, 2, 0))
+
+      #unormalize labels
+      labels = labels[i].numpy()
+      labels = labels*50.0+100
+
+      plt.subplot(row,col,i+1)
+      plt.imshow(np.squeeze(image), cmap='gray')
+      plt.scatter(labels[:, 0], labels[:, 1], s=20, marker='.', c='m')
+      plt.axis('off')
